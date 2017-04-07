5da9bbca23fc0c2ebb7ce25902cbee4501478881
ifournight <ifournight@gmail.com>
2017-04-07 15:39:29 +0800

    Todo display due date

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,14 @@
 class Todo < ApplicationRecord
   belongs_to :creator, class_name: 'User', optional: true
   belongs_to :project
-  
+
   validates :title, presence: true
+
+  def over_due?
+    unless deadline.nil?
+      return deadline < Time.zone.now
+    end
+
+    false
+  end
 end
